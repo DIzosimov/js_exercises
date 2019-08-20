a6874c48c76e0846cbbb4fb3050d2a332a578493
David_Izosimov <David.Izosimov@gmail.com>
2019-08-20 12:16:12 +0200

    Pushing js exercises to repo

---
+++ for_loop.js
@@ -0,0 +1,8 @@
+myArray = ["David", "Axel", "Daniel"]
+emptyArray = []
+
+
+for (let n = 0; n < myArray.length; n++) {
+emptyArray.push(myArray[n]);
+};
+console.log(emptyArray[n]);
---
+++ simple_stuff.js
@@ -0,0 +1,14 @@
+simpleArray = [1, 2, 3]
+simpleArray[1] // returns 2 from the array
+
+typeof 123/12 //gives "number" as type
+typeof "things in quotes" //returns "string"
+typeof undefined //returns undefined
+
+hour = 10
+if (hour < 18) {
+    value = true
+    } else {
+    value = false
+    };
+    // returns true since hour is set to 10
