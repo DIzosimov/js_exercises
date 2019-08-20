1d32cd94d230fb6a84d042eabbb9cf5539c1d4c1
David_Izosimov <David.Izosimov@gmail.com>
2019-08-20 14:45:57 +0200

    pushing the remainders of exercises to repo

---
+++ functions.js
@@ -0,0 +1,11 @@
+// Writing function that returns my name
+function functionato(name){
+    console.log(name)
+};
+functionato("Divad")
+
+// Function that returns First + Last name in one string
+function functionato(name, lastname){
+    console.log(name, lastname)
+};
+functionato("Divad", "Izosimov")
